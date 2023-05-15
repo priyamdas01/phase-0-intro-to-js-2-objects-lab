79c1edf51d6bfdd06bca46006574334c7b53f8f9
Priyam Das <domidas01@gmail.com>
2023-05-15 16:51:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name: "priyam", streetAddress: "32 bethel church"};
+function updateEmployeeWithKeyAndValue(emp, empKey, empValue){
+    const updatedEmp = {...emp}
+    updatedEmp[empKey] = empValue;
+    return updatedEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, empKey, empValue){
+    emp[empKey] = empValue;
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+    const updatedEmp = {...emp}
+    delete updatedEmp[key];
+    return updatedEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+    delete emp[key];
+    return emp;
+}
